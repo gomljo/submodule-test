34595ab8d6cb442cd31c5d028c75ddc04eb999b9
gomljo <gomljo0307@naver.com>
2023-09-05 19:12:08 +0900

    add submodule

---
+++ main-module/src/main/java/com/example/mainmodule/domain/User.java
@@ -0,0 +1,24 @@
+package com.example.mainmodule.domain;
+
+import ch.qos.logback.core.joran.spi.NoAutoStart;
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+}
